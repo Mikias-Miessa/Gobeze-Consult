d9cc9640251abcd2d4cd00379c27ad35a94ce7d4
hanna kebede <se.hanna.kebede@gmail.com>
2024-02-26 15:21:16 +0300

    feature2

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from 'next/image';
 import Hero from './(root)/page';
+import Feature from "@/components/Feature"
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <Hero />
+      {/* <Hero /> */}
+      <Feature/>
     </main>
   );
 }
