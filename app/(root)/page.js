d2efd67dd069d96ba4a31a1a74c44245e1bf32f7
Dagim Asnake <dagimasnake1@gmail>
2024-02-26 15:18:44 +0300

    feat: added second section

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Landing from '@/components/home/hero';
+import SecondSec from '@/components/home/SecondSec';
 
 const Hero = () => {
   return (
     <div>
       <Landing />
+      <SecondSec />
     </div>
   );
 };
