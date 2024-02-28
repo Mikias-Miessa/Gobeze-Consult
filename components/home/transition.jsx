a65b926e3db1ad9f7c8ec8959def7de2b3f65481
hanna kebede <se.hanna.kebede@gmail.com>
2024-02-28 11:08:42 +0300

    footer

--- components/home/transition.jsx
+++ components/home/transition.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Transition() {
   return (
     <div>
-        <blockquote className='px-80 mb-20 bg-gray-100  text-2xl py-8 text-gray-700 font-extralight leading-relaxed'>“ Articulate what we stand for, the work we’ve done so far, what we are doing currently and what we envision to be “</blockquote>
+        <blockquote className='md:px-80 px-5 md:mb-20 mb-4 bg-gray-100  md:text-2xl text-xl md:py-8 py-2 text-gray-700 font-extralight leading-relaxed'>“ Articulate what we stand for, the work we’ve done so far, what we are doing currently and what we envision to be “</blockquote>
     </div>
   )
 }
