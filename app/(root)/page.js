74cbebbd53a21a4d9c81854a1d12150080dd09a7
Dagim Asnake <dagimasnake1@gmail>
2024-02-28 14:11:17 +0300

    feat: add contact us page

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import Landing from '@/components/home/hero';
-import FirstSec from "@/components/home/firstSec";
+import FirstSec from '@/components/home/firstSec';
 import SecondSec from '@/components/home/SecondSec';
-import ThirdSec from "@/components/home/thirdSec";
+import ThirdSec from '@/components/home/thirdSec';
 import Benefit from '@/components/home/benefit';
 import LogoSection from '@/components/home/logoSection';
 import Testimonial from '@/components/home/Testimonial';
+import ContactUs from '@/components/home/contactUs';
 
 const Hero = () => {
   return (
     <div>
       <Landing />
-      <FirstSec/>
+      <FirstSec />
       <SecondSec />
-      <ThirdSec/>
+      <ThirdSec />
       <Benefit />
       <div className='flex justify-center pt-28 bg-black'>
         <LogoSection />
       </div>
       <Testimonial />
+      <div className='flex justify-center'>
+        <ContactUs />
+      </div>
     </div>
   );
 };
