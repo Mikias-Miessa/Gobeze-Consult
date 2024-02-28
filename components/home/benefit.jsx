87346fd3e99a9f089829e7b70497a278bdc57561
hanna kebede <se.hanna.kebede@gmail.com>
2024-02-28 14:59:14 +0300

    changes mapping:

--- components/home/benefit.jsx
+++ components/home/benefit.jsx
@@ -3,65 +3,52 @@ import Image from 'next/image';
 import ImgFea from '@/public/images/girl_behind.png';
 import SectionTitle from '../shared/sectionTitle';
 
+const jobsData = [
+  {
+    id: 1,
+    title: 'Present the Job1',
+    description: 'Lorem ipsum amet consectetur elit dolor sit amet consectetur elit.',
+  },
+  {
+    id: 2,
+    title: 'Present the Job2',
+    description: 'Lorem ipsum dolor amet consectetur elit sit adipisicing elit amet consectetur elit.',
+  },
+  {
+    id: 3,
+    title: 'Present the Job3',
+    description: 'Lorem ipsum dolor sit amet consectetur sit adipisicing elit amet consectetur elit.',
+  },
+  {
+    id: 4,
+    title: 'Present the Job4',
+    description: 'Lorem ipsum dolor sit amet consectetur elit.',
+  },
+];
+
 function Feature() {
   return (
     <div>
-      {/* Feature 2 */}
       <div className='bg-black'>
         <div className='grid grid-cols-1 md:grid-cols-5 gap-2 pb-10 md:pl-10 md:mx-20 mx-10'>
           <div className='md:col-span-2 mt-10 md:m mb-4 md:mb-24 h-96 w-full md:w-auto'>
-            <Image alt='benfit' src={ImgFea} className='h-100 w-full md:w-96' />
+            <Image alt='benefit' src={ImgFea} className='h-100 w-full md:w-96' />
           </div>
 
           <div className='text-white md:col-span-3 mt-3'>
             <div>
-              {/* <h1 className='text-white md:mt-20 mt-20 font-extrabold text-2xl'>THE BENEFITS</h1>
-              
-              <div className='text-orange'>
-              <hr className='w-14 h mt-4' style={{ borderColor: 'orange', borderWidth: '3px' }}/>
-              </div> */}
               <SectionTitle color={'white'} title={'THE BENEFITS'} />
 
               <div className='grid grid-cols-1 md:grid-cols-2 mt-20'>
-                <div className='md:mr-10'>
-                  <div>
-                    <h1 className='text-xl mb-4 font-semibold'>
-                      Present the Job1
-                    </h1>
-                    <p className='text-gray-400'>
-                      Lorem ipsum amet consectetur elit dolor sit amet
-                      consectetur elit.
-                    </p>
+                {jobsData.map((job) => (
+                  <div key={job.id} className='md:mr-10'>
+                    <div>
+                      <h1 className='text-xl mb-4 font-semibold'>{job.title}</h1>
+                      <p className='text-gray-400'>{job.description}</p>
+                    </div>
+                    <div className='mt-10'></div>
                   </div>
-                  <div className='mt-10'>
-                    <h1 className='text-xl mb-4 font-semibold'>
-                      Present the Job2
-                    </h1>
-                    <p className='text-gray-400'>
-                      Lorem ipsum dolor amet consectetur elit sit adipisicing
-                      elit amet consectetur elit.
-                    </p>
-                  </div>
-                </div>
-                <div className='md:mr-10'>
-                  <div className=''>
-                    <h1 className='text-xl md:mt-0 mt-4 mb-4 font-semibold'>
-                      Present the Job3
-                    </h1>
-                    <p className='text-gray-400'>
-                      Lorem ipsum dolor sit amet consectetur sit adipisicing
-                      elit amet consectetur elit .
-                    </p>
-                  </div>
-                  <div className='mt-10'>
-                    <h1 className='text-xl mb-4 font-semibold'>
-                      Present the Job4
-                    </h1>
-                    <p className='text-gray-400'>
-                      Lorem ipsum dolor sit amet consectetur elit.
-                    </p>
-                  </div>
-                </div>
+                ))}
               </div>
             </div>
           </div>
--- components/home/thirdSec.jsx
+++ components/home/thirdSec.jsx
@@ -7,79 +7,61 @@ import Community3 from '@/public/images/community_three.png';
 import Community4 from '@/public/images/community_four.png';
 import SectionTitle from '../shared/sectionTitle';
 
+const featuresData = [
+  {
+    id: 1,
+    imageSrc: Community1,
+    description:
+      'Present the job matching services that present the job matching services that.',
+  },
+  {
+    id: 2,
+    imageSrc: Community2,
+    description:
+      'Present the job matching services that present the job matching services that.',
+  },
+  {
+    id: 3,
+    imageSrc: Community3,
+    description:
+      'Present the job matching services that present the job matching services that.',
+  },
+  {
+    id: 4,
+    imageSrc: Community4,
+    description:
+      'Present the job matching services that present the job matching services that.',
+  },
+];
+
 function Feature3() {
   return (
     <div>
       <div className='grid grid-cols-1 md:grid-cols-2 gap-0 md:m-20'>
         <div className='text-white grid md:grid-cols-2 grid-cols-1 mx-4'>
-          <div>
-            <div className='bg-black px-5 pb-3 pt-5 m-5 '>
-              <div className='w-28 h-28 bg-white mx-auto my-auto rounded-full'>
-                <Image
-                  src={Community1}
-                  alt='community'
-                  className='mx-auto my-auto pt-4'
-                />
+          {featuresData.map((feature) => (
+            <div key={feature.id}>
+              <div className='bg-black px-5 pb-3 pt-5 m-5 '>
+                <div className='w-28 h-28 bg-white mx-auto my-auto rounded-full'>
+                  <Image
+                    src={feature.imageSrc}
+                    alt='community'
+                    className='mx-auto my-auto pt-4'
+                  />
+                </div>
+                <p className='text-gray-400 text-center pt-4'>
+                  {feature.description}
+                </p>
               </div>
-              <p className='text-gray-400 text-center pt-4'>
-                Present the job matching services that present the job matching
-                services that.
-              </p>
             </div>
-
-            <div className='bg-black px-5 pb-3 pt-5 m-5 mt-10 '>
-              <div className='w-28 h-28 bg-white mx-auto my-auto rounded-full'>
-                <Image
-                  src={Community2}
-                  alt='community'
-                  className='mx-auto my-auto pt-4'
-                />
-              </div>
-              <p className='text-gray-400 text-center pt-4'>
-                Present the job matching services that present the job matching
-                services that.
-              </p>
-            </div>
-          </div>
-
-          <div>
-            <div className='bg-black px-5 pb-3 pt-5 m-5'>
-              <div className='w-28 h-28 bg-white mx-auto my-auto rounded-full'>
-                <Image
-                  src={Community3}
-                  alt='community'
-                  className='mx-auto my-auto pt-4'
-                />
-              </div>
-              <p className='text-gray-400 text-center pt-4'>
-                Present the job matching services that present the job matching
-                services that.
-              </p>
-            </div>
-
-            <div className='bg-black px-5 pb-3 pt-5 m-5 mt-10'>
-              <div className='w-28 h-28 bg-white mx-auto my-auto rounded-full'>
-                <Image
-                  src={Community4}
-                  alt='community'
-                  className='mx-auto my-auto pt-4'
-                />
-              </div>
-              <p className='text-gray-400 text-center pt-4'>
-                Present the job matching services that present the job matching
-                services that.
-              </p>
-            </div>
-          </div>
+          ))}
         </div>
 
         <div className='ml-10 mx-4 mt-5'>
-          {/* <h1 className='text-3xl font-extrabold '>THIRD SECTION FEATURES</h1>
-                <div className='h-1 w-16 bg-orange-500 mt-1'></div> */}
           <SectionTitle title={'THIRD SECTION FEATURES'} color={'black'} />
-          <p className='text- text-gray-800 mt-6'>
+          <p className='text-gray-800 mt-6'>
             Present the job matching services that connect talent with creative
-            industry opportunties loream epsum epsum opportunties loream epsum
+            industry opportunities loream epsum epsum opportunities loream epsum
             epsum.
           </p>
           <button className='mb-16 mt-10 text-xl px-8 py-2 bg-gradient-to-r from-gray-100 to-black rounded'>
