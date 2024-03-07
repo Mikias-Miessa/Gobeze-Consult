b82607164ddb71c01b0a434208f5e55d3147a474
hanna kebede <se.hanna.kebede@gmail.com>
2024-03-07 11:07:35 +0300

    social support

--- app/(root)/community/page.jsx
+++ app/(root)/community/page.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Community from '@/components/community/community'
+import SocialSupport from "@/components/community/social_support"
 
 const page = () => {
   return (
     <div>
       <Community />
+      <SocialSupport/>
     </div>
   )
 }
