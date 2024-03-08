8961b6676796332de5845e5e66776e0b2be21258
hanna kebede <se.hanna.kebede@gmail.com>
2024-03-08 14:40:45 +0300

    insights

---
+++ app/(root)/insights/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import First from "@/components/insights/firstsec"
+import Insights from "@/components/insights/insights"
+
+const page = () => {
+    return (
+      <div>
+        <First/>
+        <Insights/>
+      </div>
+    )
+  }
+  
+  export default page
---
+++ components/insights/firstsec.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import SectionTitle from '../shared/sectionTitle';
+
+
+function Firstsec() {
+  return (
+    <div
+    className='bg-cover bg-center bg-no-repeat min-h-screen flex justify-center items-center w-full overflow-hidden'
+      style={{
+        backgroundImage: `url("/images/insights/bgInsights.png")`,
+      }}>
+        <div>
+        <SectionTitle className="text-" title={'Insights'} color={'white'} />
+        </div>
+    </div>
+  )
+}
+
+export default Firstsec
