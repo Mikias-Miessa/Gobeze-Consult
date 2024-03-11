05232d6bd2683196188596e798232ef60af3e744
hanna kebede <se.hanna.kebede@gmail.com>
2024-03-11 08:33:02 +0300

    .

--- components/insights/firstsec.jsx
+++ components/insights/firstsec.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import Image from "next/image"
 
 
 function Firstsec() {
   return (
     <div
-    className='bg-cover bg-center bg-no-repeat min-h-screen flex justify-center items-center w-full overflow-hidden'
+    className='bg-cover bg-center bg-no-repeat min-h-screen flex justify-center items-center w-full h-fit overflow-hidden'
       style={{
-        backgroundImage: `url("/images/insights/bgInsights.png")`,
+        backgroundImage: `url("/images/insights/bgInsights2.png")`, 
       }}>
        <div className=''>
-      <h1 className="font-extrabold text-2xl md:text-6xl text-white">INSIGHTS</h1>
+      <h1 className="font-extrabold text-2xl md:text-8xl text-white">INSIGHTS</h1>
       <div className='justify-center item-center h-1 w-20 bg-orange-500 mt-6 ml-20'></div>
     </div>
     </div>
