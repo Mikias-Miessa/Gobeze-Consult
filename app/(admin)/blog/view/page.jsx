d398f44810928ebcbf5642dbcd0fa1bfe57101c8
Dagim Asnake <dagimasnake1@gmail>
2024-03-13 11:17:30 +0300

    fix: minor edit in admin view

--- app/(admin)/blog/view/page.jsx
+++ app/(admin)/blog/view/page.jsx
@@ -39,21 +39,17 @@ const ViewBlog = () => {
             key={item._id}
             className='max-w-sm bg-white border border-gray-200 rounded-lg shadow'
           >
-            <a href='#'>
-              <Image
-                className='rounded-t-lg'
-                src={item.image}
-                width={500}
-                height={500}
-                alt='the image'
-              />
-            </a>
+            <Image
+              className='rounded-t-lg'
+              src={item.image}
+              width={500}
+              height={500}
+              alt='the image'
+            />
             <div className='p-5'>
-              <a href='#'>
-                <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900'>
-                  {item.title}
-                </h5>
-              </a>
+              <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900'>
+                {item.title}
+              </h5>
               <p className='mb-3 font-normal text-gray-700'>{item.content}</p>
               <div className='flex justify-center gap-10'>
                 <Link
@@ -72,41 +68,6 @@ const ViewBlog = () => {
             </div>
           </div>
         ))}
-
-        {/* {blogs.map((item) => (
-          <div
-            key={item._id}
-            className='block max-w-[18rem] bg-white text-surface shadow-secondary-1 m-4'
-          >
-            <h1 className='text-5xl'>{item.title}</h1>
-            <div className='relative overflow-hidden bg-cover bg-no-repeat'>
-              <Image
-                className='mb-7'
-                src={item.image}
-                width={100}
-                height={100}
-                alt='the image'
-              />
-            </div>
-            <div className='mb-7'>
-              <p className='text-base'>{item.content}</p>
-            </div>
-            <div className='flex justify-center'>
-              <Link
-                href={`/blog/edit/${item._id}`}
-                className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2 focus:outline-none focus:shadow-outline'
-              >
-                Edit
-              </Link>
-              <button
-                onClick={() => handleDelete(item._id)}
-                className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
-              >
-                Delete
-              </button>
-            </div>
-          </div>
-        ))} */}
       </div>
     </div>
   );
