bd27fea6b845678d520b691af3597611b809a800
Dagim Asnake <dagimasnake1@gmail>
2024-03-13 11:03:24 +0300

    fix: edited blog card in admin page

--- app/(admin)/blog/view/page.jsx
+++ app/(admin)/blog/view/page.jsx
@@ -32,12 +32,51 @@ const ViewBlog = () => {
   }
 
   return (
-    <div className='flex justify-center items-center bg-white'>
+    <div className='flex justify-center items-center py-10'>
       <div className='flex flex-wrap justify-center items-center gap-10'>
         {blogs.map((item) => (
           <div
             key={item._id}
-            className='block max-w-[18rem] text-surface shadow-secondary-1 m-4'
+            className='max-w-sm bg-white border border-gray-200 rounded-lg shadow'
+          >
+            <a href='#'>
+              <Image
+                className='rounded-t-lg'
+                src={item.image}
+                width={500}
+                height={500}
+                alt='the image'
+              />
+            </a>
+            <div className='p-5'>
+              <a href='#'>
+                <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900'>
+                  {item.title}
+                </h5>
+              </a>
+              <p className='mb-3 font-normal text-gray-700'>{item.content}</p>
+              <div className='flex justify-center gap-10'>
+                <Link
+                  href={`/blog/edit/${item._id}`}
+                  className='inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300'
+                >
+                  Edit
+                </Link>
+                <button
+                  onClick={() => handleDelete(item._id)}
+                  className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+
+        {/* {blogs.map((item) => (
+          <div
+            key={item._id}
+            className='block max-w-[18rem] bg-white text-surface shadow-secondary-1 m-4'
           >
             <h1 className='text-5xl'>{item.title}</h1>
             <div className='relative overflow-hidden bg-cover bg-no-repeat'>
@@ -67,7 +106,7 @@ const ViewBlog = () => {
               </button>
             </div>
           </div>
-        ))}
+        ))} */}
       </div>
     </div>
   );
