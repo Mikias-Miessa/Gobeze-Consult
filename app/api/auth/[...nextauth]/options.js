1cb6283b46cdcbad92982386aa1cd14936fa43b2
Dagim Asnake <dagimasnake1@gmail>
2024-03-15 15:54:09 +0300

    fix: adde authentication for users

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -1,16 +1,12 @@
-// import type { NextAuthOptions } from "next-auth";
-import axios from 'axios';
-import bcrypt from 'bcrypt';
 import CredentialsProvider from 'next-auth/providers/credentials';
+import bcrypt from 'bcrypt';
+import dbConnect from '@/utils/db';
+import User from '@/models/User';
+
 export const options = {
   providers: [
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
       name: 'Credentials',
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
         username: {
           label: 'Username',
@@ -24,28 +20,18 @@ export const options = {
         },
       },
       async authorize(credentials, req) {
-        // console.log(credentials);
-        // const res = await axios.post('/api/user', credentials, {
-        //   headers: { 'Content-Type': 'application/json' },
-        // });
-        // console.log(res);
-        // const user = await res.json();
+        await dbConnect();
 
-        // // If no error and we have user data, return it
-        // if (res.ok && user) {
-        //   return user;
-        // }
-        // // Return null if user data could not be retrieved
-        // return null;
-        const user = { username: 'admin', password: '1234' };
+        const username = credentials.username;
+        const user = await User.findOne({ username });
+
         if (
-          user.username === credentials.username &&
-          user.password === credentials.password
+          !user ||
+          !(await bcrypt.compare(credentials.password, user.password))
         ) {
-          return user;
-        } else {
           return null;
         }
+        return user;
       },
     }),
   ],
