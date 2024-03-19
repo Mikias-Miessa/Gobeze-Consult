ead2e3e9967828d86491a70782bee63e031809dc
Dagim Asnake <dagimasnake1@gmail>
2024-03-19 16:05:35 +0300

    fix: add bg image to 404

--- app/not-found.js
+++ app/not-found.js
@@ -3,23 +3,30 @@ import Link from 'next/link';
 
 const notFound = () => {
   return (
-    <div className='min-h-screen flex flex-grow items-center justify-center'>
-      <div className='p-8 text-center max-w-md'>
-        <h1 className='mb-3 text-9xl font-extrabold'>
-          <span className='drop-shadow-[2px_2px_var(--tw-shadow-color)] shadow-orange-500'>
-            404
-          </span>
-        </h1>
-        <p className='font-bold text-5xl'>PAGE NOT FOUND</p>
-        <p className='mt-4'>
-          The page you are looking for might have been removed had its name
-          changed or is temporarily unavailable.
-        </p>
-        <div className='mt-5 w-40 mx-auto'>
-          <div className='border-inline border-2'>
-            <Link href='/'>
-              <p className='border p-2'>Home Page</p>
-            </Link>
+    <div
+      className='bg-cover bg-center w-full overflow-hidden'
+      style={{
+        backgroundImage: `url("/images/404/404_page.png")`,
+      }}
+    >
+      <div className='min-h-screen flex flex-grow items-center justify-center'>
+        <div className='p-8 text-center max-w-md'>
+          <h1 className='mb-3 text-9xl font-extrabold'>
+            <span className='drop-shadow-[2px_2px_var(--tw-shadow-color)] shadow-orange-500'>
+              404
+            </span>
+          </h1>
+          <p className='font-bold text-5xl'>PAGE NOT FOUND</p>
+          <p className='mt-4'>
+            The page you are looking for might have been removed had its name
+            changed or is temporarily unavailable.
+          </p>
+          <div className='mt-5 w-40 mx-auto'>
+            <div className='border-inline border-2'>
+              <Link href='/'>
+                <p className='border p-2'>Home Page</p>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
