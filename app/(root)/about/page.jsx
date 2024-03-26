79c8831269972e997747f232190a616dc87cede7
Dagim Asnake <dagimasnake1@gmail>
2024-03-26 10:16:23 +0300

    feat: add about main page

--- app/(root)/about/page.jsx
+++ app/(root)/about/page.jsx
@@ -4,11 +4,13 @@ import OurVision from '@/components/about/ourVission';
 import OurVisionSecond from '@/components/about/ourVisionSecond';
 import Training from '@/components/about/training';
 import SomeSection from '@/components/about/someSection';
+import AboutSecond from '@/components/about/aboutSecond';
 
 const page = () => {
   return (
     <div>
       <About />
+      <AboutSecond />
       <OurVision />
       <OurVisionSecond />
       <SomeSection/>
