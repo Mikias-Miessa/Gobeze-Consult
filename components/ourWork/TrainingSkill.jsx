cbbb19b7897d3a6327a064e9f6bf9b0e052e01c5
Dagim Asnake <dagimasnake1@gmail>
2024-03-29 13:56:55 +0300

    fix: edited work page

---
+++ components/ourWork/TrainingSkill.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Image from 'next/image';
+import Rectangle from '@/public/images/work/training1.png';
+import Rectangle2 from '@/public/images/work/traing2.png';
+import Rectangle3 from '@/public/images/work/traing3.png';
+
+const TrainingSkill = () => {
+  return (
+    <div className='bg-black py-36'>
+      <div className='md:flex items-center max-w-screen-lg mx-auto gap-14 overflow-hidden md:px-0 px-14'>
+        <div className='md:w-1/2'>
+          <Image src={Rectangle2} alt='aster' className='' />
+          <div className='flex gap-3'>
+            <Image src={Rectangle} alt='aster' className='md:w-full w-36' />
+            <Image src={Rectangle3} alt='aster' className='md:w-full w-36' />
+          </div>
+        </div>
+        <div className='md:w-1/2'>
+          <div className='mb-5'>
+            <h1 className='text-5xl md:text-7xl font-bold text-center md:text-start bg-gradient-to-r from-gray-300 via-gray-200 to-gray-100 inline-block text-transparent bg-clip-text'>
+              Why We Do It
+            </h1>
+          </div>
+          <div className='text-xl text-gray-400'>
+            Our aim at Gobeze Consult is firmly anchored in the conviction that
+            good people deserve to succeed. Here, we highlight the concrete
+            actions we do to assist people and organisations that are working to
+            improve their communities and beyond. Our organisation assists
+            individuals in obtaining cataract surgery in an effort to positively
+            impact society and provide scholarships for courses to female
+            students. We enjoy designing workspaces that are cosy and
+            reminiscent of the people we care about.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TrainingSkill;
--- components/ourWork/workspace.jsx
+++ components/ourWork/workspace.jsx
@@ -1,37 +1,47 @@
 import React from 'react';
-import Image from "next/image";
-import Rectangle from '@/public/images/work/workspace1.png'
-import Rectangle2 from '@/public/images/work/workspace2.png'
-import Rectangle3 from '@/public/images/work/workspace3.png'
-
-const workSpaceData = [
-  {
-    id: 0,
-    image: Rectangle,
-  }
-]
+import Image from 'next/image';
+import Rectangle10 from '@/public/images/work/Rectangle10.png';
+import Rectangle9 from '@/public/images/work/Rectangle9.png';
+import Rectangle8 from '@/public/images/work/Rectangle8.png';
 
 function WorkSpace() {
   return (
-    <div id='workspace'> 
-      <div className='grid md:grid-cols-3 grid-cols-1 md:mx-36 md:gap-8 my-3 mx-5'>
+    <div id='workspace'>
+      <div className='grid md:grid-cols-3 grid-cols-1 md:mx-36 md:gap-3 my-3 mx-5'>
         <div className='md:mt-16 mt-5 md:mb-10 mb-5'>
-          <h1 className='text-xl font-bold pb-4'>Present</h1>
-          <h3 className='text-3xl font-extrabold'>Provision of workspace, meeting and conference solutions</h3>
-          <p className='text-xl font-light mt-4'>Present the job matching services that connect talent with creative industry opportunities.</p>
+          <h1 className='text-xl font-bold pb-2'>Services</h1>
+          <h3 className='text-2xl font-extrabold'>
+            Provision of workspace, meeting and conference solutions
+          </h3>
+          <p className='text-lg font-light mt-4'>
+            From flexible workspaces tailored to your needs to state-of-the-art
+            meeting rooms and flawless conference setups, we ensure seamless and
+            productive environments for your business endeavors.
+          </p>
+          <div className='text-center bg-white border border-black mt-5 w-1/3'>
+            <button className='px-0 py-1 text-black mx-auto'>
+              Learn More
+            </button>
+          </div>
         </div>
         <div>
-          <Image src={Rectangle} alt='imgWorkSpace1' className='md:h-full h-80 md:mt-0 mt-2 md:mb-0 mb-2'/>
+          <Image
+            src={Rectangle8}
+            alt='imgWorkSpace1'
+            className='md:h-full h-80 md:mt-0 mt-2 md:mb-0 mb-2'
+          />
         </div>
         <div className=''>
-        <Image src={Rectangle2} alt='imgWorkSpace2' className='md:mb-8 mb-2'/>
-        <Image src={Rectangle3} alt='imgWorkSpace3' className=""/>
+          <Image
+            src={Rectangle9}
+            alt='imgWorkSpace2'
+            className='md:mb-10 mb-2'
+          />
+          <Image src={Rectangle10} alt='imgWorkSpace3' className='' />
         </div>
-
       </div>
-
     </div>
-  )
+  );
 }
 
 export default WorkSpace;
