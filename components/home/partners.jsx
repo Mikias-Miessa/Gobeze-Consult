d971da2a1623c4be2e19a839306bf67f8b945de9
hanna kebede <se.hanna.kebede@gmail.com>
2024-04-08 12:59:12 +0300

    partner integrated

--- components/home/partners.jsx
+++ components/home/partners.jsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react';
 import Image from 'next/image';
 import SectionTitle from '@/components/shared/sectionTitle';
@@ -6,38 +7,66 @@ import Rectangle2 from '@/public/images/firstconsult2.png';
 import Logo1 from '@/public/images/partnerLogo1.png';
 import Logo2 from '@/public/images/britishC.png';
 import { FiArrowRight } from 'react-icons/fi';
+import { Bars } from 'react-loader-spinner';
+import {getAllPartners,selectAllPartners,selectLoading} from '@/store/partnerSlice';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 
 function Partners() {
-  const data = [
-    {
-      id: 1,
-      name: "First Consult",
-      imageLogo: Logo1,
-      description: 'First Consult is a leading economic development consulting firm implementing projects in Ethiopia.',
-      image: Rectangle1,
-    },
-    {
-      id: 2,
-      name: "British Council",
-      imageLogo: Logo2,
-      description: 'First Consult is a leading economic development consulting firm implementing projects in Ethiopia.',
-      image: Rectangle2,
-    },
-  ];
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllPartners());
+  }, []);
+  const partners = useSelector(selectAllPartners);
+  const loading = useSelector(selectLoading);
+  console.log(partners);
 
+  // const data = [
+  //   {
+  //     id: 1,
+  //     name: "First Consult",
+  //     imageLogo: Logo1,
+  //     description: 'First Consult is a leading economic development consulting firm implementing projects in Ethiopia.',
+  //     image: Rectangle1,
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "British Council",
+  //     imageLogo: Logo2,
+  //     description: 'First Consult is a leading economic development consulting firm implementing projects in Ethiopia.',
+  //     image: Rectangle2,
+  //   },
+  // ];
+  if (loading) {
+    return (
+      <div className=' h-screen bg-white flex justify-center items-center'>
+        <Bars
+          height='40'
+          width='40'
+          color='#FF7F00'
+          ariaLabel='bars-loading'
+          wrapperStyle={{}}
+          wrapperClass=''
+          visible={true}
+        />
+      </div>
+    );
+  }
   return (
     <div className='bg-black md:px-28 px-5 md:py-20 py-5'>
       <div>
         <SectionTitle title={'OUR PARTNERS'} color={'white'} />
+        
       </div>
       <div className='grid md:grid-cols-2 grid-cols-1 gap-16 text-white mt-14'>
-        {data.map((partner) => (
-          <div key={partner.id} className='bg-white text-black border border-orange-500 grid grid-cols-5'>
+        {partners.map((item) => (
+          <div key={item.id} className='bg-white text-black border border-orange-500 grid grid-cols-5'>
             <div className='col-span-3 h-full flex flex-col justify-between'>
               <div>
-                <Image src={partner.imageLogo} alt='companyLogo' className='pl-20 pt-4' />
+                <img src={item.logo} alt='companyLogo' className='pl-20 pt-4' />
                 <h3 className='ml-5 mr-2 mt-4 pt-5 pl-5'>
-                  <span className='font-extrabold'>{partner.name}</span> is a leading economic development consulting firm implementing projects in Ethiopia.
+                  <span className='font-extrabold'>{item.name}</span> is a leading economic development consulting firm implementing projects in Ethiopia.
                 </h3>
               </div>
               <button className='bg-black text-white flex items-center w-full px-5 py-4 pl-20'>
@@ -45,9 +74,10 @@ function Partners() {
               </button>
             </div>
             <div className='col-span-2'>
-              <Image src={partner.image} alt={`partner${partner.id}`} className='h-full' />
+              <img src={item.image && item.image} alt={`partner${item.id}`} className='h-full' />
             </div>
           </div>
+          
         ))}
       </div>
     </div>
