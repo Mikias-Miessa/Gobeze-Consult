39fc78109989eb35784a02fca6e4942f784665dd
Dagim Asnake <dagimasnake1@gmail.com>
2024-04-19 09:37:34 +0300

    fix: community start up page

--- components/community/startup.jsx
+++ components/community/startup.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
-import Image from "next/image"
-import Startupimg from "@/public/images/community/startup.png"
+import React from 'react';
+import Image from 'next/image';
+import Startupimg from '@/public/images/community/startup.png';
 
 function Startup() {
   return (
     <div className='bg-black'>
-        <div className='grid grid-cols-2 pl-20 pb-10 h-4/6'>
-            <div className='md:pt-16 pt-5 ml-10'>
-              <h1 className='text-gray-200 font-light text-3xl'> <span className='font-bold text-gray-50 text-6xl'>F</span>rom formulating policy insights
-                 to actively engaging with the community and influencing the trajectory of the next generation of Ethiopian businesses, we are an active member of the startup eco system in Ethiopia.
-              </h1>
-              <button className='border border-white font-bold text-sm px-10 py-3 text-white mt-10'>Learn More</button>
-            </div>
-            <div>
-              <Image src={Startupimg} className='w-fit ml-16'/>
-            </div>
+      <div className='px-10 md:py-0 py-10 max-w-screen-lg mx-auto'>
+        <div className='flex items-center gap-5'>
+          <div className='md:w-1/2 w-full'>
+            <h1 className='text-gray-200 font-light text-3xl'>
+              {' '}
+              <span className='font-bold text-gray-50 text-6xl'>F</span>rom
+              formulating policy insights to actively engaging with the
+              community and influencing the trajectory of the next generation of
+              Ethiopian businesses, we are an active member of the startup eco
+              system in Ethiopia.
+            </h1>
+            <button className='border border-white font-bold text-sm px-10 py-3 text-white mt-10'>
+              Learn More
+            </button>
+          </div>
+          <div className='w-1/2 hidden md:block'>
+            <Image src={Startupimg} alt='start up' className='' />
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Startup
+export default Startup;
